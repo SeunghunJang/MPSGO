9afa40394aa2e91eddc90ec996732317122bb159
SeunghunJang <99380164+SeunghunJang@users.noreply.github.com>
2025-03-31 20:46:12 +0900

    Add files via upload

---
+++ Gen_molecular_pairs_cal/mol_4_7.xyz/qsub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=mol-rlx       # Job name
+
+#SBATCH --nodelist=node02
+##SBATCH --exclusive             # Use of full Memory in one job at only one node
+
+#SBATCH -p batch
+#SBATCH --cpus-per-task=1        # Number of cores per MPI rank 
+#SBATCH --nodes=1                # Number of nodes
+#SBATCH --ntasks-per-node=4      # How many tasks on each node
+#SBATCH --output=log.out         # Standard output and error log
+
+DIR=$PWD
+echo $DIR
+
+~/cal_tmp/10_orca/orca_5_0_4_linux_x86-64_shared_openmpi411/orca   $DIR/input.inp   >   std.out
